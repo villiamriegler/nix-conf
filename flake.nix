f0530a61233a8c6bdd9bf1db8c8438b8c7d2ba0a
villiamriegler <ville@riegler.se>
2026-02-21 14:06:16 +0100

    Set up hosts

--- flake.nix
+++ flake.nix
@@ -10,16 +10,36 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
+  let 
+    # Directory containing host definitions. 
+    # A host definition defines the system where the configuration should run.
+    hostsDir = ./hosts;
+
+    # Extracts all the hostnames from the hostsDir
+    hostNames = builtins.attrNames (
+      nixpkgs.lib.filterAttrs ( _ : type: type == "directory" ) (builtins.readDir hostsDir)
+    );
+
+    # Creates a system configuration from a host definition 
+    mkHost = {hostname} : nixpkgs.lib.nixosSystem {
+      modules = [ 
+        (hostsDir + "/${hostname}/configuration.nix")
+	home-manager.nixosModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.villiamr = ./home.nix;
+        }
+      ];
+    };
+
+    # Host systems defined from the host definitions
+    hosts = builtins.listToAttrs (map (hostname : {
+        name = hostname;
+        value = mkHost { hostname = hostname; };
+      }) hostNames
+    );
+  in
   {
-	nixosConfigurations.villiamr = nixpkgs.lib.nixosSystem {
-		modules = [ 
-			./configuration.nix
-			home-manager.nixosModules.home-manager {
-				home-manager.useGlobalPkgs = true;
-				home-manager.useUserPackages = true;
-				home-manager.users.villiamr = ./home.nix;
-			}
-		];
-	};
+    nixosConfigurations = hosts;
   };
 }
