426409e37df6f5bd9b7d58de7ff3cf4a60cd24cf
Villiam Riegler <ville@riegler.se>
2026-04-13 13:23:31 +0200

    actually fix blink

--- dotfiles/nvim/lua/config/lsp.lua
+++ dotfiles/nvim/lua/config/lsp.lua
@@ -72,5 +72,5 @@ require('blink.cmp').setup {
     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
     -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    fuzzy = { implementation = "prefer_rust" }
 }
