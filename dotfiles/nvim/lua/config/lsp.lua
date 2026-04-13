5485c197156a88d080a2f120688d1b5f8ada3a66
Villiam Riegler <ville@riegler.se>
2026-04-13 13:03:25 +0200

    pin blink version backwards

--- dotfiles/nvim/lua/config/lsp.lua
+++ dotfiles/nvim/lua/config/lsp.lua
@@ -1,6 +1,6 @@
 vim.pack.add {
     'https://github.com/neovim/nvim-lspconfig',
-    'https://github.com/saghen/blink.cmp',
+    { src = 'https://github.com/saghen/blink.cmp', version = 'v1' },
     'https://github.com/rafamadriz/friendly-snippets',
     'https://github.com/fang2hou/blink-copilot',
     'https://github.com/zbirenbaum/copilot.lua',
